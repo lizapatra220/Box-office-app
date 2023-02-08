d2c1ef0c6ee7de9eca608b484ffccbf0b149b7bb
lizapatra220 <lizapatra220@gmail.com>
2023-02-08 16:47:29 +0530

    search bar created

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React ,{useState}from 'react'
 import MainPageLayout from "../Components/MainPageLayout"
 const Home = () => {
-  return <MainPageLayout>This is a home page</MainPageLayout>
+    const [input, setinput] = useState('');
+    const onSearchValue =()=>{
+        fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+        .then(r=>r.json())
+        .then(result=>{
+            console.log(result);
+        })
+    }
+   
+   const  onChangeValue =(ev)=>{
+        setinput(ev.target.value);
+
+    }
+    const onKeyDown =(ev)=>{
+        if(ev.keyCode===13){
+            onSearchValue();
+        }
+    }
+   
+  return <MainPageLayout>
+    <input type="text" onChange={onChangeValue} value={input} onKeyDown ={onKeyDown}/>
+    <button type="text" onClick={onSearchValue}>Search</button>
+  </MainPageLayout>
     
 }
 
