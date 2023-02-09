f0793bbef8640a07df690575a57dc5092cbdde50
lizapatra220 <lizapatra220@gmail.com>
2023-02-09 12:16:23 +0530

    loading and error message

--- src/Pages/Show.js
+++ src/Pages/Show.js
@@ -5,14 +5,35 @@ import { getApi } from "../misc/config";
 const Show = () => {
     const {id} = useParams();
     const [show, setshow] = useState(null)
+    const [isLoading, setisLoading] = useState(true);
+    const [error, seterror] = useState(null);
     useEffect(() => {
+        let isMounted =true;
         getApi(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(result=>{
-            setshow(result);
+                if(isMounted){
+                    setshow(result);
+                    setisLoading(false);
+
+                }
+        }).catch((err)=>{
+            if(isMounted){
+                seterror(err.message);
+                setisLoading(false);
+
+            }
         })
+        return ()=>{
+            isMounted(false)
+        }
 
      
     }, [id])
-    console.log('show',show)
+    if(isLoading){
+        return <div>Data  is being loaded</div>
+    }
+    if(error){
+        return <div>Error occurred: {error}</div>
+    }
     
    
   return (
