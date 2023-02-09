e63b1fd2c8a3be7e0ace6e6473acf676e0417711
lizapatra220 <lizapatra220@gmail.com>
2023-02-09 16:52:25 +0530

    replace useState with useReducer

--- src/Pages/Show.js
+++ src/Pages/Show.js
@@ -1,24 +1,45 @@
-import React,{useState,useEffect} from 'react'
+import React,{useReducer,useEffect} from 'react'
 import  {useParams} from "react-router-dom";
 import { getApi } from "../misc/config";
 
+ const initialState ={
+    show:null,
+    isLoading:true,
+    error:null
+ }
+ const reducer =(prevState,action)=>{
+    switch (action.type) {
+        case 'Fetch_Success':{
+            return {isLoading:false,error:null,show:action.show}
+        }
+        case 'Fetch_Failure':{
+            return {...prevState,isLoading:false,error:action.error}
+        }
+    
+    
+        default:
+            return prevState;
+    }
+ }
+
 const Show = () => {
     const {id} = useParams();
-    const [show, setshow] = useState(null)
-    const [isLoading, setisLoading] = useState(true);
-    const [error, seterror] = useState(null);
+   const [{isLoading,show,error},dispatch] = useReducer(reducer,initialState);
+    // const [show, setshow] = useState(null)
+    // const [isLoading, setisLoading] = useState(true);
+    // const [error, seterror] = useState(null);
     useEffect(() => {
         let isMounted =true;
         getApi(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(result=>{
                 if(isMounted){
-                    setshow(result);
-                    setisLoading(false);
+                    dispatch({type:"Fetch_Success",show:result})
+                   
 
                 }
         }).catch((err)=>{
             if(isMounted){
-                seterror(err.message);
-                setisLoading(false);
+                dispatch({type:"Fetch_Failure",error:err.message})
+                
 
             }
         })
@@ -28,6 +49,7 @@ const Show = () => {
 
      
     }, [id])
+    console.log('show',show)
     if(isLoading){
         return <div>Data  is being loaded</div>
     }
